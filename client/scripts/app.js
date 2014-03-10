d7154204eda5efa438d13bd08683ee624b9a9f7c
Juan Marin <benjamin.v.stone@gmail.com>
2014-03-10 14:35:34 -0700

    Added auto chat refresh

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,26 +1,37 @@
-// YOUR CODE HERE:
-$.ajax({
-  // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  contentType: 'application/json',
-  data: {
-    order:"-createdAt",
-    where:{"roomname":"4chan"}
-  },
-  success: function (data) {
-    appendDiv(data.results);
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
+
+var roomName = "4chan";
 
 var appendDiv = function(messages){
+  $('#chatbox').html('');
   _.each(messages, function(item, i){
-    var $node = $("<div class='chatMessage'></div>").text(item.username.substring(0, 25) +": "+item.text.substring(0,140));
+    var aUsername = item.username || 'guest' ;
+    var aMessage = item.text || 'default text' ;
+    var $node = $("<div class='chatMessage'></div>").text(aUsername.substring(0, 25) +": "+aMessage.substring(0,140));
     $("#chatbox").append($node);
-    console.log("message", item);
   });
 };
+
+var refreshChat = function(roomName){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    data: {
+      order:"-createdAt",
+      where:{"roomname":roomName}
+    },
+    success: function (data) { appendDiv(data.results);},
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+}
+ 
+// INIT
+refreshChat(roomName);
+// REFRESH
+ setInterval(function(){
+  refreshChat(roomName);
+ }, 1000);
