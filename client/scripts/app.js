a0de47ce2987f7520eda0884e1e97522c9985ea9
Juan Marin <benjamin.v.stone@gmail.com>
2014-03-11 17:34:22 -0700

    intermediate cleanin-up state, factorizing

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -34,9 +34,10 @@ app = {
   /*****************************************************************************/
   /* CONFIG                                                                    */
   /*****************************************************************************/
-  roomName: "4chan",
+  roomname: "4chan",
   userName: 'Guest',
   apiUrl: 'https://api.parse.com/1/classes/chatterbox',
+  url: 'https://api.parse.com/1/classes/chatterbox',
   roomTimeStamp: {} ,
   messageTimeStamp: "2011-03-11T09:34:08.256Z",
 
@@ -69,14 +70,6 @@ app = {
       }
     });
   },
-  /*****************************************************************************/
-  /* ACCESSORS */
-  /*****************************************************************************/
-  setRoomName:function(name){
-    if(roomName !== name){
-      roomName = name;
-    }
-  },
 
   /*****************************************************************************/
   /* AJAX */
@@ -88,7 +81,7 @@ app = {
       data: JSON.stringify({
       'username': userName,
       'text': aMessage,
-      'roomname': roomName
+      'roomname': app.roomname
     }),
       contentType: 'application/json',
       success: function (data) {
@@ -101,60 +94,49 @@ app = {
     });
   },
 
-  getChatRooms: function(aCallback){
-    aCallback = aCallback || function(item){console.log(item)}; 
-    $.ajax({
-      url: apiUrl,
-      type: 'GET',
-      contentType: 'application/json',
+  fetch:function(arg){
+    var ajaxJson = { 
+      url: app.url, 
+      type: 'GET', 
+      contentType: 'application/json', 
       data: {order:"-createdAt",limit:800},
-      success: function (data) {
-        // AIM #1: GET LIST OF LAST ROOMS; RESULT: AN ORDERED ARRAY OF ROOMS 
+      error: function (data) {
+        console.error('chatterbox: Failed to get message');
+      }
+    };
+    if(arg['callback']){ // GET CHAT ROOM LIST 
+      arg['callback'] = arg['callback'] || function(item){console.log(item)}; 
+      
+      ajaxJson['success'] = function (data) {
         var roomList = _.filter(_.uniq(_.pluck(data.results, 'roomname')), function(item){
           return !!item;
         });
-        aCallback(roomList);
-        // AIM #2: LAST TIME PER ROOM
+        arg['callback'](roomList);
         _.each(data.results, function(item){
           if(!roomTimeStamp[item.roomname] || (new Date(roomTimeStamp[item.roomname])) < (new Date(item.createdAt))){
             roomTimeStamp[item.roomname] = item.createdAt;
             console.log("Updated timestamp", item.roomname, new Date(roomTimeStamp[item.roomname]), new Date(item.createdAt));
           }
         });
-      },
-      error: function (data) {
-        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message');
       }
-    })
-  },
-  getMessages: function(roomName, fullRefresh){
-    if(fullRefresh){
-      messageTimeStamp = "2011-03-11T09:34:08.256Z";
-    }
-    $.ajax({
-      url: apiUrl,
-      type: 'GET',
-      contentType: 'application/json',
-      data: {
-        order:"-createdAt",
-        limit:200,
-        where:JSON.stringify({"roomname":roomName})
-      },
-      success: function (data) { 
+    } else { // GET MESSAGE 
+      if(arg['fullRefresh']){
+        messageTimeStamp = "2011-03-11T09:34:08.256Z";
+      } 
+      console.log(arg['roomname']);
+      ajaxJson['data']['where'] = JSON.stringify({"roomname":arg['roomname']});
+      ajaxJson['success'] = function (data) { 
         var results = _.filter(data.results, function(item){
           item.username += " "+item.roomname;
           return (new Date(item.createdAt)) > (new Date(messageTimeStamp));
         });
-        if(results.length > 0 && results[0].roomname === roomName){
+        if(results.length > 0 && results[0].roomname === app.roomname){
           messageTimeStamp = results[0].createdAt;
-          appendDiv(results, fullRefresh); 
+          appendDiv(results, arg['fullRefresh']); 
         }
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to send message');
-      }
-    })
+      };
+    }
+    $.ajax(ajaxJson);
   },
 
   /*****************************************************************************/
@@ -192,12 +174,12 @@ app = {
     $('#sidebar #roomlist').html('');
 
     _.each(rooms, function(room, i){
-      $("#sidebar #roomlist").append($("<li class='roomName'></li>").text(room));
+      $("#sidebar #roomlist").append($("<li class='roomname'></li>").text(room));
     });
-    $("li.roomName").on('click', function(){
-      app.setRoomName($(this).text());
-      getMessages(roomName, true);
-      $('input[name=roomname]').val(roomName);
+    $("li.roomname").on('click', function(){
+      app.roomname = $(this).text();
+      app.fetch({roomname:app.roomname, fullRefresh: true});
+      $('input[name=roomname]').val(app.roomname);
       
     });
   },
@@ -208,19 +190,19 @@ app = {
   event:function(){
     $("input[name=message]").keypress(function(event){
       if(event.keyCode === 13){
-        app.setRoomName($('input[name=roomname]').val());
+        app.roomname = $('input[name=roomname]').val();
         userName = $('input[name=username]').val();
         send($(this).val());
         $(this).val("");
-        getMessages(roomName);
-        getChatRooms(refreshRoomList);
+        app.fetch({roomname:app.roomname});
+        app.fetch({callback:refreshRoomList});
       }
     });
     $("input[name=roomname]").keypress(function(event){
       if(event.keyCode === 13){
-        app.setRoomName($('input[name=roomname]').val());
-        getMessages(roomName,true);
-        getChatRooms(refreshRoomList);
+        app.roomname = $('input[name=roomname]').val();
+        app.fetch({roomname:app.roomname, fullRefresh: true});
+        app.fetch({callback:refreshRoomList});
       }
     });
   }
@@ -228,10 +210,7 @@ app = {
 
 /*****************************************************************************/
 var send = app.send ; 
-var getMessages = app.getMessages ; 
-var getChatRooms = app.getChatRooms ; 
-var roomName= app.roomName,
-  userName= app.userName,
+var userName= app.userName,
   apiUrl= app.apiUrl,
   roomTimeStamp= app.roomTimeStamp,
   messageTimeStamp= app.messageTimeStamp;
@@ -245,12 +224,12 @@ var escapeMe = template.escape;
 $(document).ready(function(){
   app.init();
   setupSpamMacros();
-  getMessages(roomName);
-  getChatRooms(refreshRoomList);
+  app.fetch({roomname:app.roomname});
+  app.fetch({callback:refreshRoomList});
   setInterval(function(){
-    getMessages(roomName);
+    app.fetch({roomname:app.roomname});
   }, 1000);
   setInterval(function(){
-    getChatRooms(refreshRoomList);
+    app.fetch({callback:refreshRoomList});
   }, 1000);
 });
